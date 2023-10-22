df5b4c8012f22c232fd23e88831fcca243857132
dilrong <dilrong@dilrong.com>
2023-10-22 21:37:33 +0900

    feat: 카테고리 정리

--- src/app/novels/components/NovelFilter.tsx
+++ src/app/novels/components/NovelFilter.tsx
@@ -8,16 +8,24 @@ interface Props {}
 const NovelFilter = ({}: Props) => {
   return (
     <>
-      <p className="text-sm pt-2">Category Tags</p>
-      <div className="w-full flex gap-4 pt-1 pb-4">
+      <div className="w-full flex items-center gap-4 pt-1 pb-4">
+        <Link href="/novels/category/romance">
+          <CategoryTag name="Fantasy" />
+        </Link>
         <Link href="/novels/category/fun">
-          <CategoryTag name="FUN" />
+          <CategoryTag name="Fun" />
         </Link>
         <Link href="/novels/category/horror">
-          <CategoryTag name="HORROR" />
+          <CategoryTag name="Horror" />
+        </Link>
+        <Link href="/novels/category/romance">
+          <CategoryTag name="Romance" />
         </Link>
         <Link href="/novels/category/sad">
-          <CategoryTag name="SAD" />
+          <CategoryTag name="Sad" />
+        </Link>
+        <Link href="/novels/category/romance">
+          <CategoryTag name="SF" />
         </Link>
       </div>
     </>
