7c11aae73d88977eea14013abb8d9669c2de99f3
dilrong <dilrong@dilrong.com>
2023-11-03 22:12:43 +0900

    fix: analytics

--- src/app/_components/organisms/Headers.tsx
+++ src/app/_components/organisms/Headers.tsx
@@ -5,7 +5,6 @@ import Logo from '@/app/_components/molecules/Logo'
 import NavMobile from '@/app/_components/molecules/Nav/NavMobile'
 import NavPc from '@/app/_components/molecules/Nav/NavPc'
 import NavContainer from '@/app/_components/molecules/Nav/NavContainer'
-import Analytics from '../molecules/Analytics'
 import MetaInfo from '../atoms/MetaInfo'
 
 function Headers() {
@@ -18,7 +17,6 @@ function Headers() {
   return (
     <>
       <Suspense>
-        <Analytics />
         <MetaInfo />
       </Suspense>
       <NavContainer>
