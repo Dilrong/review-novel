a68bc6350bae8712c38a43b958ff9de31841a4f0
dilrong <dilrong@dilrong.com>
2023-11-06 22:23:37 +0900

    feat: 설문을 위한 CTA 버튼 추가

---
+++ src/app/_components/molecules/CtaButton.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Link from 'next-intl/link'
+
+interface Props {
+  text: string
+  href: string
+}
+
+function CtaButton({ text, href }: Props) {
+  return (
+    <Link className="fixed bottom-2 right-2" href={href} target="_blank">
+      <button
+        type="button"
+        className="rounded-full bg-neutral-800 px-4 py-3 text-center text-2xl text-white shadow-xl hover:bg-neutral-900 focus:outline-none focus:ring-0 focus:ring-primary"
+      >
+        {text}
+      </button>
+    </Link>
+  )
+}
+
+export default CtaButton
